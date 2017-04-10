4f8e725f4cd218ce13aaef2c83fac70ac12e65d1
Babatunde Adeyemi <tundewrites@gmail.com>
2017-04-10 10:59:41 +0100

    Accumulate arguments to args, and call the fetchCharacter function

--- src/gameOfThronesControl.js
+++ src/gameOfThronesControl.js
@@ -1,1 +1,13 @@
 var fetchCharacter = require('./gameOfThrones.js');
+
+var processData = process.argv.slice(2);
+
+var args = [];
+
+processData.forEach(function(value) {
+	args.push(value);
+});
+
+
+
+fetchCharacter.fetchCharacter(args[0]);
